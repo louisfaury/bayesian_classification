f0b3a1ae7f28619d5014933dbf15e84b07d2583d
louisfaury <l.faury@hotmail.fr>
2017-03-14 15:24:27 +0100

    Adds sample mini batch

--- utils/sample_mini_batch.m
+++ utils/sample_mini_batch.m
@@ -1,3 +1,14 @@
 function [mb, t] = sample_mini_batch(mb_size, data, in_size)
-    
+% <============ HEADER =============>
+% @brief    : sample mini-batch (with replacement) from the dataset
+% @params   : mb_size <- size of the mini-batch
+%             data <- dataset
+%             in_size <- input vector dimension 
+% @returns  : mb <- sampled mini batched
+%             t <- corresponding targets
+% <============ HEADER =============>
+
+[mb,ix] = datasample(data(:,in_size),mb_size);
+t = data(ix,in_size+1);
+
 end
