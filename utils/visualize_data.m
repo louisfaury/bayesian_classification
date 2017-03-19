88f57b1d3fbb54078f7134195aff318b82aee8d7
louisfaury <l.faury@hotmail.fr>
2017-03-19 22:00:09 +0100

    Adds scatter plto data visualization for dataset

--- utils/visualize_data.m
+++ utils/visualize_data.m
@@ -19,15 +19,15 @@ proj_data = data*proj_matrix';
 
 %plots
 if (retained_dim==2)
-    figure;
+    figure('units','normalized','outerposition',[0 0 1 1])
     hold on;
     for i=1:size(dataset,1)
         if (dataset(i,input_size+1))
-            color = [0.9, 0.1, 0.2];
-            m = plot(proj_data(i,1),proj_data(i,2),'.','MarkerSize',15,'Color',color);
+            color = [1, 0.5, 0.4];
+            subplot(1,2,1); hold on; m = plot(proj_data(i,1),proj_data(i,2),'.','MarkerSize',15,'Color',color);
         else
-            color = [0.1, 0.9, 0.2];
-            b = plot(proj_data(i,1),proj_data(i,2),'.','MarkerSize',15,'Color',color);
+            color = [0.4, 0.5, 0.9];
+            subplot(1,2,1); hold on; b = plot(proj_data(i,1),proj_data(i,2),'.','MarkerSize',15,'Color',color);
         end
     end
 end
@@ -38,4 +38,24 @@ ylabel('$e_2$','Interpreter','latex');
 legend([m,b],'Malign','Benign');
 
 
+%% Hist plots 
+m = [];
+b = [];
+
+for i=1:size(dataset,1)
+    % really unefficient but only way to have nice legends
+    for j = 1:input_size
+       if (dataset(i,input_size+1))
+           m = [m;[j,dataset(i,j)+0.2*randn]];
+       else
+           b = [b;[j,dataset(i,j)+0.2*randn]];
+       end
+    end
+end
+
+subplot(1,2,2); hold on; scatter(m(:,1),m(:,2),60,'MarkerFaceColor',[1, 0.5, 0.4],'MarkerEdgeColor',[1, 0.5, 0.4],'MarkerFaceAlpha',0.5,'MarkerEdgeAlpha',0.5);
+subplot(1,2,2); hold on; scatter(b(:,1),b(:,2),60,'MarkerFaceColor',[0.4, 0.5, 0.9],'MarkerEdgeColor',[0.4, 0.5, 0.9],'MarkerFaceAlpha',0.5,'MarkerEdgeAlpha',0.5);
+legend('Malign','Benign');
+xlabel('Input index');
+title('Scatter plot representation of data');
 end
