ef6f945c71c025abaf9135e05827b2af4187d9c8
louisfaury <l.faury@hotmail.fr>
2017-03-21 16:45:33 +0100

    Adds bias in output computing

--- utils/compute_output.m
+++ utils/compute_output.m
@@ -1,8 +1,9 @@
-function y = compute_output(activation_function, w, input, feature)
+function y = compute_output(activation_function, w, b, input, feature)
 % <============ HEADER =============>
 % @brief    : computes the output [yn = a(w'*phi(xn))]
 % @params   : activation_function <- string for desired activation 
 %             w <- current point estimate of learned regressor
+%             b <- learned bias 
 %             input 
 %             feature <- string for desired feature 
 % @returns  : y <- output
@@ -15,7 +16,7 @@ switch feature
         error('Unknwown feature');
 end
 
-z = input*w;
+z = input*w + b*ones(size(input,1),1);
 
 switch activation_function
     case 'logistic_sigmoid'
