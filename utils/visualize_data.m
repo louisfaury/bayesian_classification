9760280aa4365b57f7293071439c9de8a432ecd6
louisfaury <l.faury@hotmail.fr>
2017-04-23 22:04:03 +0200

    Adapts vizu to final dataset

--- utils/visualize_data.m
+++ utils/visualize_data.m
@@ -14,7 +14,7 @@ data = data ./ sqrt(var(data));
 empirical_cov = (data-mean(data))'*(data-mean(data));
 [V,D] = eigs(empirical_cov,input_size,'sm');
 retained_dim = 2;
-%explained_variance = trace(D(1:retained_dim,1:retained_dim))/ trace(D); %=0.76
+%explained_variance = trace(D(1:retained_dim,1:retained_dim))/ trace(D); %70%
 proj_matrix = V(:,1:retained_dim)';
 proj_data = (data-mean(data))*proj_matrix';
 
@@ -43,14 +43,31 @@ legend([m,b],'Positive','Negative');
  ixp = find(dataset(:,input_size+1));
  ixn = find(~dataset(:,input_size+1));
  figure('units','normalized','outerposition',[0 0 1 1]);
- subplot(3,3,1); histogram(dataset(ixp,1),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,1),'FaceColor','green'); title('Numbers of pregnancy'); legend('Positive','Negative');
- subplot(3,3,2); histogram(dataset(ixp,2),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,2),'FaceColor','green'); title('Plasma Glucose Concentration'); legend('Positive','Negative');
- subplot(3,3,3); histogram(dataset(ixp,3),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,3),'FaceColor','green'); title('Diastolic blood pressure'); legend('Positive','Negative');
- subplot(3,3,4); histogram(dataset(ixp,4),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,4),'FaceColor','green'); title('Triceps skin fold thickness'); legend('Positive','Negative');
- subplot(3,3,5); histogram(dataset(ixp,5),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,5),'FaceColor','green'); title('2-Hour serum insulin'); legend('Positive','Negative');
- subplot(3,3,6); histogram(dataset(ixp,6),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,6),'FaceColor','green'); title('Body mass index'); legend('Positive','Negative');
- subplot(3,3,7); histogram(dataset(ixp,7),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,7),'FaceColor','green'); title('iabetes pedigree function'); legend('Positive','Negative');
- subplot(3,3,8); histogram(dataset(ixp,8),11,'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,8),11,'FaceColor','green'); title('Age'); legend('Positive','Negative');
+ subplot(6,4,1); histogram(dataset(ixp,1),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,1),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,2); histogram(dataset(ixp,2),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,2),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,3); histogram(dataset(ixp,3),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,3),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,4); histogram(dataset(ixp,4),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,4),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,5); histogram(dataset(ixp,5),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,5),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,6); histogram(dataset(ixp,6),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,6),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,7); histogram(dataset(ixp,7),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,7),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,8); histogram(dataset(ixp,8),11,'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,8),11,'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,9); histogram(dataset(ixp,9),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,9),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,10); histogram(dataset(ixp,10),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,10),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,11); histogram(dataset(ixp,11),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,11),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,12); histogram(dataset(ixp,12),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,12),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,13); histogram(dataset(ixp,13),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,13),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,14); histogram(dataset(ixp,14),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,14),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,15); histogram(dataset(ixp,15),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,15),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,16); histogram(dataset(ixp,16),11,'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,16),11,'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,17); histogram(dataset(ixp,17),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,17),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,18); histogram(dataset(ixp,18),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,18),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,19); histogram(dataset(ixp,19),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,19),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,20); histogram(dataset(ixp,20),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,20),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,21); histogram(dataset(ixp,21),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,21),'FaceColor','green'); legend('Positive','Negative');
+ subplot(6,4,22); histogram(dataset(ixp,22),'FaceColor','red','FaceAlpha',0.8); hold on; histogram(dataset(ixn,22),'FaceColor','green'); legend('Positive','Negative');
+
+ 
+ 
 
 
 end
